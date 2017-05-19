189c1281f47e0ac7d55a3c442f2c679aa640272d
koszuta <koszutan94@gmail.com>
2017-05-18 23:36:29 -0500

    Added multithreading to radius neighbors and BF

--- source.cpp
+++ source.cpp
@@ -22,12 +22,12 @@
 #include <thread>
 #include <mutex>
 
-// GLM header file ***
+// GLM header file
 #include <glm/glm.hpp>
 
 using namespace std;
 
-// Location of OFF file input ***
+// Location of OFF file input
 static const char* inputPath = "fandisk_noise.off";
 
 // Output location
@@ -36,20 +36,20 @@ static const char* outputPath = "";
 static int showFace = 0;
 
 
-// Define constants for algorithm ***
-static const int NF = 3;
-static const int NV = 20;
+// Define constants for algorithm
+static const int NF = 4;
+static const int NV = 50;
 static const float LAMBDA = 0.05f;
 
 // User defined sigma c (radius) and sigma s (standard deviation
-// of intesity differneces between center and area) ***
+// of intesity differneces between center and area)
 static float sigC = 0.0f;
 static float sigS = 0.0f;
 
 static int userFace = -1;
 static unordered_set<int> facesWithinRadius;
 
-// Position of light ***
+// Position of light
 glm::vec3 light_pos = { 1.0f, 1.0f, 1.0f };
 const GLfloat PURPLE[3] = { 0.5f, 0.0f, 1.0f };
 const GLfloat WHITE[3] = { 1.0f, 1.0f, 1.0f };
@@ -83,7 +83,12 @@ float xdiff = 0.0f;
 float ydiff = 0.0f;
 
 
-mutex mtx;
+thread inputThread;
+mutex inputMtx;
+bool shouldSmooth = false;
+
+int numThreads = thread::hardware_concurrency();
+mutex normMtx, vertMtx, vNeighborMtx, fNeighborMtx, radNeighborMtx, edgeNeighborMtx;
 
 
 enum transformation
@@ -93,7 +98,7 @@ enum transformation
 	TRANSLATE
 };
 
-// Updated Mesh object with glm::vec3 types ***
+// Updated Mesh object with glm::vec3 types
 struct Mesh {
 	int nv;
 	int nf;
@@ -115,15 +120,15 @@ static bool* isFaceVisited;
 
 
 // Get user defined center face and radius
-void getRadius(Mesh* mesh)
+void getUserInput(Mesh* mesh)
 {
 	string temp = "";
 	while (strcmp(temp.c_str(), "y"))
 	{
 		temp = "";
-		mtx.lock();
+		inputMtx.lock();
 		userFace = -1;
-		mtx.unlock();
+		inputMtx.unlock();
 		cout << "Enter face at center of area expected to be smooth, in range [0, " << mesh->nf << ")" << endl;
 
 		int face;
@@ -134,9 +139,9 @@ void getRadius(Mesh* mesh)
 			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 			continue;
 		}
-		mtx.lock();
+		inputMtx.lock();
 		userFace = face;
-		mtx.unlock();
+		inputMtx.unlock();
 		cout << "Use face " << face << "? (y/n)" << endl;
 		cin >> temp;
 	}
@@ -147,9 +152,9 @@ void getRadius(Mesh* mesh)
 		temp = "";
 		sigC = 0.0f;
 		cout << "Enter radius of the smooth area..." << endl;
-		mtx.lock();
+		inputMtx.lock();
 		facesWithinRadius.clear();
-		mtx.unlock();
+		inputMtx.unlock();
 
 		float radius;
 		if (!(cin >> radius) || radius <= 0)
@@ -159,16 +164,16 @@ void getRadius(Mesh* mesh)
 			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 			continue;
 		}
-		mtx.lock();
+		inputMtx.lock();
 		sigC = radius;
-		mtx.unlock();
+		inputMtx.unlock();
 		for (int i = 0; i < mesh->nf; i++)
 		{
 			if (i != userFace && sigC > glm::distance(mesh->centroid[i], mesh->centroid[userFace]))
 			{
-				mtx.lock();
+				inputMtx.lock();
 				facesWithinRadius.insert(i);
-				mtx.unlock();
+				inputMtx.unlock();
 			}
 		}
 		cout << "Use radius " << radius << "? (y/n)" << endl;
@@ -176,45 +181,50 @@ void getRadius(Mesh* mesh)
 	}
 
 	printf("\nface = %d, radius = %f\n", userFace, sigC);
+
+	shouldSmooth = true;
 }
 
-// Spatial smoothing function ***
+// Spatial smoothing function
 float smoothing_func(float x)
 {
 	return exp(-x*x / (2*sigC*sigC));
 }
 
-// Influence function ***
+// Influence function
 float influence_func(float x)
 {
 	return exp(-x*x / (2*sigS*sigS));
 }
 
-// Intensity difference between surface normals ***
+// Intensity difference between surface normals
 float intensity_diff(glm::vec3 ni, glm::vec3 nj)
 {
 	return glm::dot(ni, ni - nj);
 }
 
-// Apply bilateral filter to smooth surface normals ***
-void bilateral_filter(Mesh *mesh)
+void doBF(int low, int high, Mesh *mesh, glm::vec3 *normals)
 {
-	// Create array to hold new normals
-	glm::vec3* newNormals = (glm::vec3*) malloc(sizeof(glm::vec3) * mesh->nf);
-
-	for (int i = 0; i < mesh->nf; i++)
+	for (int i = low; i < high; i++)
 	{
 		// Get normal and centroid of face i
+		normMtx.lock();
 		glm::vec3 ni = mesh->normal[i];
+		normMtx.unlock();
 		glm::vec3 ci = mesh->centroid[i];
 
 		glm::vec3 top(0.0f);
 		float bottom = 0.0f;
 
-		for (const auto& j : vertexRadiusFaces[i])
+		radNeighborMtx.lock();
+		unordered_set<int> neighbors = vertexRadiusFaces[i];
+		radNeighborMtx.unlock();
+		for (auto& j : neighbors)
 		{
 			// Get normal and centroid of face j
+			normMtx.lock();
 			glm::vec3 nj = mesh->normal[j];
+			normMtx.unlock();
 			glm::vec3 cj = mesh->centroid[j];
 
 			// Calculate Ws and Wc
@@ -225,27 +235,54 @@ void bilateral_filter(Mesh *mesh)
 			bottom += wc * ws;
 		}
 
-		// Normalize smoothed normal
-		glm::vec3 smoothedNormal = top / bottom;
-		smoothedNormal = glm::normalize(smoothedNormal);
-		newNormals[i] = smoothedNormal;
+		// Add normalized smoothed normal to list
+		normMtx.lock();
+		normals[i] = glm::normalize(top / bottom);
+		normMtx.unlock();
+	}
+}
+
+// Apply bilateral filter to smooth surface normals
+void bilateral_filter(Mesh *mesh)
+{
+	// Create array to hold new normals
+	glm::vec3* newNormals = (glm::vec3*) malloc(sizeof(glm::vec3) * mesh->nf);
+
+	if (numThreads > 1)
+	{
+		vector<thread> threads;
+		int chunk = mesh->nf / numThreads;
+		for (int i = 0; i < numThreads; i++)
+		{
+			threads.push_back(thread(doBF, i*chunk, (i < numThreads - 1) ? (i + 1)*chunk : mesh->nf, mesh, newNormals));
+		}
+		for (auto& t : threads)
+		{
+			if (t.joinable())
+			{
+				t.join();
+			}
+		}
+	}
+	else
+	{
+		doBF(0, mesh->nf, mesh, newNormals);
 	}
 
 	// Copy new array of normals to mesh array of normals
-	mtx.lock();
+	normMtx.lock();
 	memcpy(mesh->normal, newNormals, sizeof(glm::vec3) * mesh->nf);
-	mtx.unlock();
+	normMtx.unlock();
 	free(newNormals);
 }
 
-// Adjust vertices using Least Squares Error method ***
+// Adjust vertices using Least Squares Error method
 void lse_correction(Mesh *mesh)
 {
 	glm::vec3* newVertices = (glm::vec3*) malloc(sizeof(glm::vec3) * mesh->nv);
 
 	for (int i = 0; i < mesh->nv; i++)
 	{
-		//printf("LSE: Vertex %d\n", i);
 		glm::vec3 sum;
 		for (int j : vertexNeighborVertices[i])
 		{
@@ -261,9 +298,9 @@ void lse_correction(Mesh *mesh)
 	}
 
 	// Copy new vertices to mesh vertices
-	mtx.lock();
+	vertMtx.lock();
 	memcpy(mesh->vertex, newVertices, sizeof(glm::vec3) * mesh->nv);
-	mtx.unlock();
+	vertMtx.unlock();
 	free(newVertices);
 }
 
@@ -273,7 +310,7 @@ void smooth(Mesh *mesh)
 	chrono::time_point<chrono::system_clock> start;
 	chrono::duration<double> duration;
 
-	// Smooth the surface normals ***
+	// Smooth the surface normals
 	for (int i = 0; i < NF; i++)
 	{
 		start = chrono::system_clock::now();
@@ -283,7 +320,7 @@ void smooth(Mesh *mesh)
 		duration = chrono::system_clock::now() - start;
 		printf("BF duration = %f\n", duration);
 	}
-	// Adjust the vertices ***
+	// Adjust the vertices
 	for (int i = 0; i < NV; i++)
 	{
 		start = chrono::system_clock::now();
@@ -295,14 +332,20 @@ void smooth(Mesh *mesh)
 	}
 }
 
+mutex sdMtx;
+float sum = 0.0f;
+int N = 0;
 
-void getRadiusNeighbors(Mesh *mesh, float radius)
+void doRadiusNeighbors(int low, int high, Mesh *mesh, float radius)
 {
-	float sum = 0.0f;
-	int N = 0;
-	for (int fi = 0; fi < mesh->nf; fi++)
+	bool* isFaceVisited = (bool*)malloc(sizeof(bool*) * mesh->nf);
+	bool* isVertexVisited = (bool*)malloc(sizeof(bool*) * mesh->nv);
+
+	for (int fi = low; fi < high; fi++)
 	{
 		deque<int> facesToVisit;
+		for (int f = 0; f < mesh->nf; f++) isFaceVisited[f] = false;
+		for (int v = 0; v < mesh->nv; v++) isVertexVisited[v] = false;
 
 		// Initially add center face to list
 		facesToVisit.push_back(fi);
@@ -319,14 +362,18 @@ void getRadiusNeighbors(Mesh *mesh, float radius)
 				if (fi != fj)
 				{
 					// Add neighbor to center face's list
-					vertexRadiusFaces[fi].insert(fj);
+					radNeighborMtx.lock();
+						vertexRadiusFaces[fi].insert(fj);
+					radNeighborMtx.unlock();
 
 					// Accumulate SD if current face is the user selected face
 					// and neighboring face is within user selected radius
 					if (fi == userFace && distance < radius)
 					{
-						sum += pow(intensity_diff(mesh->normal[fi], mesh->normal[fj]), 2);
-						N++;
+						sdMtx.lock();
+							sum += pow(intensity_diff(mesh->normal[fi], mesh->normal[fj]), 2);
+							N++;
+						sdMtx.unlock();
 					}
 				}
 
@@ -337,7 +384,12 @@ void getRadiusNeighbors(Mesh *mesh, float radius)
 					if (!isVertexVisited[vertex])
 					{
 						isVertexVisited[vertex] = true;
-						for (int neighborFace : vertexNeighborFaces[vertex])
+
+						vNeighborMtx.lock();
+							set<int> neighbors = vertexNeighborFaces[vertex];
+						vNeighborMtx.unlock();
+
+						for (int neighborFace : neighbors)
 						{
 							if (fi != neighborFace && !isFaceVisited[neighborFace])
 							{
@@ -349,15 +401,33 @@ void getRadiusNeighbors(Mesh *mesh, float radius)
 				}
 			}
 		}
-		for (int f = 0; f < mesh->nf; f++)
+	}
+}
+
+void getRadiusNeighbors(Mesh *mesh, float radius)
+{
+	if (numThreads > 1)
+	{
+		vector<thread> threads;
+		int chunk = mesh->nf / numThreads;
+
+		for (int i = 0; i < numThreads; i++)
 		{
-			isFaceVisited[f] = false;
+			threads.push_back(thread(doRadiusNeighbors, i*chunk, (i < numThreads - 1) ? (i + 1)*chunk : mesh->nf, mesh, radius));
 		}
-		for (int v = 0; v < mesh->nv; v++)
+		for (auto& t : threads)
 		{
-			isVertexVisited[v] = false;
+			if (t.joinable())
+			{
+				t.join();
+			}
 		}
 	}
+	else
+	{
+		doRadiusNeighbors(0, mesh->nf, mesh, radius);
+	}
+
 	sigS = sqrt(sum / N);
 	printf("\nstandard deviation = %f\n\n", sigS);
 }
@@ -391,8 +461,6 @@ void doSmooth(Mesh *mesh)
 	chrono::time_point<chrono::system_clock> totalStart;
 	chrono::time_point<chrono::system_clock> start;
 	chrono::duration<double> duration;
-
-	getRadius(mesh);
 
 	totalStart = chrono::system_clock::now();
 
@@ -437,7 +505,7 @@ void addNeighboringFaces(int n, int a, int b, int c)
 }
 
 
-// Calculate surface normals with glm functions ***
+// Calculate surface normals with glm functions
 glm::vec3 normalFromVertices(Mesh *mesh, int a, int b, int c)
 {
 	glm::vec3 u = mesh->vertex[a] - mesh->vertex[b];
@@ -446,7 +514,7 @@ glm::vec3 normalFromVertices(Mesh *mesh, int a, int b, int c)
 }
 
 
-// Calculate centroids of triangle face ***
+// Calculate centroids of triangle face
 glm::vec3 calcCentroid(Mesh *mesh, int a, int b, int c)
 {
 	return (mesh->vertex[a] + mesh->vertex[b] + mesh->vertex[c]) / 3.0f;
@@ -481,7 +549,7 @@ Mesh* readPolygon()
 	surfmesh->nf = n;
 	surfmesh->ne = num;
 
-	// Allocate space for lists based on mesh size ***
+	// Allocate space for lists based on mesh size
 	surfmesh->vertex = (glm::vec3*)malloc(sizeof(glm::vec3) * surfmesh->nv);
 	surfmesh->face = (glm::ivec3*)malloc(sizeof(glm::ivec3) * surfmesh->nf);
 	surfmesh->normal = (glm::vec3*)malloc(sizeof(glm::vec3) * surfmesh->nf);
@@ -512,13 +580,13 @@ Mesh* readPolygon()
 		surfmesh->face[n].y = c;
 		surfmesh->face[n].z = d;
 
-		// Add neighbors for each vertex ***
+		// Add neighbors for each vertex
 		addNeighboringVertices(b, c, d);
 		// Add normal neighbors for each vertex
 		addNeighboringFaces(n, b, c, d);
-		// Calculate face surface normal ***
+		// Calculate face surface normal
 		surfmesh->normal[n] = normalFromVertices(surfmesh, b, c, d);
-		// Calculate triangle centroid ***
+		// Calculate triangle centroid
 		surfmesh->centroid[n] = calcCentroid(surfmesh, b, c, d);
 
 		if (a != 3)
@@ -596,37 +664,41 @@ void draw()
 	}
 	
 	for (int i = 0; i < surfmesh->nf; ++i) {
-		// Use face's surface normal for lighting ***
+		// Use face's surface normal for lighting
 		glNormal3f(surfmesh->normal[i].x, surfmesh->normal[i].y, surfmesh->normal[i].z);
 		
-		mtx.lock();
-		if (i == userFace)
+		if (inputMtx.try_lock())
 		{
-			const GLfloat HIGHLIGHT[3] = { 1.0f - PURPLE[0], 1.0f - PURPLE[1], 1.0f - PURPLE[2] };
-			glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, HIGHLIGHT);
+			if (i == userFace)
+			{
+				const GLfloat HIGHLIGHT[3] = { 1.0f - PURPLE[0], 1.0f - PURPLE[1], 1.0f - PURPLE[2] };
+				glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, HIGHLIGHT);
+			}
+			else if (facesWithinRadius.count(i) != 0)
+			{
+				glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, WHITE);
+			}
+			else
+			{
+				glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, PURPLE);
+			}
+			/*
+			if (i == showFace % surfmesh->nf)
+			{
+				const GLfloat YELLOW[3] = { 1.0f, 1.0f, 0.0f };
+				//glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, YELLOW);
+			}
+			//*/
+			inputMtx.unlock();
 		}
-		else if (facesWithinRadius.count(i) != 0)
-		{
-			glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, WHITE);
-		}
-		else
-		{
-			glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, PURPLE);
-		}
-		/*
-		if (i == showFace % surfmesh->nf)
-		{
-			const GLfloat YELLOW[3] = { 1.0f, 1.0f, 0.0f };
-			//glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, YELLOW);
-		}
-		//*/
-		mtx.unlock();
 
+		vertMtx.lock();
 		glBegin(GL_TRIANGLES);
 			glVertex3f(surfmesh->vertex[surfmesh->face[i].x].x, surfmesh->vertex[surfmesh->face[i].x].y, surfmesh->vertex[surfmesh->face[i].x].z);
 			glVertex3f(surfmesh->vertex[surfmesh->face[i].y].x, surfmesh->vertex[surfmesh->face[i].y].y, surfmesh->vertex[surfmesh->face[i].y].z);
 			glVertex3f(surfmesh->vertex[surfmesh->face[i].z].x, surfmesh->vertex[surfmesh->face[i].z].y, surfmesh->vertex[surfmesh->face[i].z].z);
 		glEnd();
+		vertMtx.unlock();
 	}
 
 	// Both FILL and LINE 
@@ -665,7 +737,7 @@ void display()
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();
 
-	// Adjust light position based on translate and scale ***
+	// Adjust light position based on translate and scale
 	GLfloat lp[3] = { scale * (light_pos.x + tx), scale * (light_pos.y + ty), scale * light_pos.z };
 	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, PURPLE);
 	glLightfv(GL_LIGHT0, GL_POSITION, lp);
@@ -795,7 +867,20 @@ void mouseMotion(int x, int y)
 
 void idle()
 {
+	if (shouldSmooth)
+	{
+		shouldSmooth = false;
+		// Make sure input thread is done
+		if (inputThread.joinable())
+		{
+			inputThread.join();
+		}
+		// Start smoothing
+		doSmooth(surfmesh);
+	}
+
 	showFace+=10;
+
 	if (!mouseDown)
 	{
 		xrot += 0.3f;
@@ -807,8 +892,9 @@ void idle()
 
 int main(int argc, char *argv[])
 {
-	thread t1(doSmooth, surfmesh);
-	//doSmooth(surfmesh);
+	printf("\nAvailable threads = %d\n\n", numThreads);
+
+	inputThread = thread(getUserInput, surfmesh);
 
 	glutInit(&argc, argv);
 
@@ -843,6 +929,5 @@ int main(int argc, char *argv[])
 	std::free(surfmesh->centroid);
 	std::free(surfmesh);
 
-	t1.join();
 	return 0;
 }
